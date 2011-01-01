65d8050af8095e5cf45109864a0ed04e0a4a7d96
Felipe Doria <felipe.doria@gmail.com>
2011-01-01 03:01:51 -0300

    First page as cover

--- document/table.rb
+++ document/table.rb
@@ -8,10 +8,12 @@ LearningMaterial.generate("table.pdf") do
   chapters_folder = File.expand_path(File.join(File.dirname(__FILE__),
                                                "chapters"))
 
+  text "Cover", :size => 50
+
   Dir.chdir(chapters_folder) do
     Dir.glob("*") do |chapter|
+      start_new_page      
       load_chapter(File.join(chapters_folder, chapter))
-      start_new_page
     end
   end
 end
